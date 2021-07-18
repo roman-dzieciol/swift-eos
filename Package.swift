b8fd2d3d478a636332c0f65a30d8a9dd45b5455c
Roman Dzieciol <roman.dzieciol@gmail.com>
2021-07-18 03:11:02 +0100

    feat: Testable version of Epic's EOSSDK framework.
    See also: https://github.com/roman-dzieciol/swift-eos-build
    
    - Public API & ABI is identical to official release, so can be used in place of standard SDK.
    - EOSSDK implementation is replaced with calls to public function pointers, which tests can customize.
    
    Provide function pointer implementations to test values that EOSSDK api receives and return custom results.
    
    NOTE: The function pointers use C calling convention, which can access only provided parameters or global references.

--- Package.swift
+++ Package.swift
@@ -11,25 +11,40 @@ let package = Package(
         .library(
             name: "SwiftEOS",
             targets: ["SwiftEOS"]),
+        .library(
+            name: "SwiftEOSWithTestableSDK",
+            targets: ["SwiftEOSWithTestableSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "TestableEOSSDK", path: "../swift-eos-test")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftEOS",
-            dependencies: ["EOSSDK"]
+            dependencies: ["EOSSDK"],
+            path: "Sources/SwiftEOS"
+        ),
+        .target(
+            name: "SwiftEOSWithTestableSDK",
+            dependencies: [.product(name: "EOSSDK", package: "TestableEOSSDK")],
+            path: "SourcesLink/SwiftEOS"
         ),
         .binaryTarget(
             name: "EOSSDK",
-            path: "EOSSDK.xcframework"
+            url: "https://github.com/roman-dzieciol/swift-eos-build/releases/download/0.0.1/EOSSDK.xcframework.zip",
+            checksum: "e04ced44f462d39819ae23d38780aa4d312ad378c601ffd1e6a88f1db3c26aae"
         ),
         .testTarget(
             name: "SwiftEOSTests",
-            dependencies: ["SwiftEOS"]),
-        
+            dependencies: ["SwiftEOS"]
+        ),
+        .testTarget(
+            name: "SwiftEOSWithTestableSDKTests",
+            dependencies: ["SwiftEOSWithTestableSDK"]
+        ),
     ]
 )
